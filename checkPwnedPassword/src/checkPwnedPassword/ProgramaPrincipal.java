9f092df4c06d5eb6f6212d5c4639b44abf03fe98
Denis <dstefanescu001@ikasle.ehu.eus>
2018-11-17 13:24:51 +0100

    The program is now more efficient.

--- checkPwnedPassword/src/checkPwnedPassword/ProgramaPrincipal.java
+++ checkPwnedPassword/src/checkPwnedPassword/ProgramaPrincipal.java
@@ -23,7 +23,7 @@ public class ProgramaPrincipal {
 		
 		try {
 			System.out.println("\nThe program is searching for your password in the pwned passwords file. Please don't abort...");
-			checkPass("passwords.txt", password);
+			checkPass("C:\\Users\\denis\\git\\passwords.txt", password);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -32,14 +32,14 @@ public class ProgramaPrincipal {
 	private static void checkPass(String pFile, String pPassword) throws IOException {
 		
 		boolean coincide = false;
-		String hashedPass = Sha1.aplicarSha1(pPassword);
+		String hashedPass = Sha1.aplicarSha1(pPassword).toUpperCase();
 		String sCurrentLine;
 		
 		try(FileReader fr = new FileReader(pFile);
 	    	    BufferedReader br = new BufferedReader(fr))
 	    	{
 			while ((sCurrentLine = br.readLine()) != null) {
-				if(sCurrentLine.equals(hashedPass)) {
+				if(sCurrentLine.contains(hashedPass)) {
 					coincide = true;
 					br.close();
 				}
@@ -47,9 +47,7 @@ public class ProgramaPrincipal {
 			
 			br.close();
 			
-	    	} catch (IOException e) {
-	    		 e.printStackTrace();
-	    	}
+	    	} catch (IOException e) {}
 		
 		if(coincide == false)
 			System.out.println("\nCongratulations! Your password wasn't found in the list!");
